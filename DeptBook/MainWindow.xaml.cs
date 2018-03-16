cb40bebfd8caad63de70ffc676f92ea588f88b23
larsholm92 <larsholm92@gmail.com>
2018-03-16 08:58:56 +0100

    Added a few lines of code

--- DeptBook/MainWindow.xaml.cs
+++ DeptBook/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DeptBook.ViewModel;
 
 namespace DeptBook
 {
@@ -23,6 +24,8 @@ namespace DeptBook
         public MainWindow()
         {
             InitializeComponent();
+            ViewModel.ViewModel vm = new ViewModel.ViewModel();
+
         }
     }
 }

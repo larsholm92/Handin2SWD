9dbc680ae24f42ced328a4e42c37d33396773941
ChrisBroberg <Christoffer.Broberg@hotmail.com>
2018-04-11 11:01:47 +0200

    Added extra example

--- Handin3SWD/DecoratorPattern/DecoratorPattern/Program.cs
+++ Handin3SWD/DecoratorPattern/DecoratorPattern/Program.cs
@@ -10,10 +10,20 @@ namespace DecoratorPattern
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("All-inclusive Audi R8");
             ICar Audi = new LEDFrontLightwAudiLaserlight(new CeramicBrake( new CruiseControl(new WheelRim(new EnlargedFuelTank(new AudiR8())))));
 
             Console.WriteLine("{0:C}",Audi.GetCost());
             Console.WriteLine(Audi.GetAccessories());
+
+            Console.WriteLine("\nAudi R8, budget version");
+
+            ICar Audib = new CruiseControl(new AudiR8());
+
+            Console.WriteLine("{0:C}", Audib.GetCost());
+            Console.WriteLine(Audib.GetAccessories());
+
+
         }
     }
 }
